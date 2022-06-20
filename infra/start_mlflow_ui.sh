6bf7fe4334b52ae15f67b3ff12cb224a87584e4b
Johannes Kiechle <hannes.kiechle@gmail.com>
2022-06-20 17:06:42 -0600

    add start script for mlflow ui

---
+++ infra/start_mlflow_ui.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#load python
+module load python/3.9
+
+#activate virtual environment
+cd && source projects/def-uofavis-ab/jkiechle/CoMA/env/bin/activate
+
+#start mlflow ui
+mlflow ui -h 0.0.0.0 -p 5000 &
