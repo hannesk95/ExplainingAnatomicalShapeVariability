ed11fc1f71ed9c9b30817f338a7585aaaf634115
Johannes Kiechle <hannes.kiechle@gmail.com>
2022-06-20 17:33:48 -0600

    update interactive job script

--- infra/start-interactive-job.sh
+++ infra/start-interactive-job.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
-<<<<<<< HEAD
-salloc --gres=gpu:1 --time=0-1:0:0 --account=def-uofavis-ab --cpus-per-task=8 --mem-per-cpu=6GB
-=======
 #allocate V100 GPU
 #salloc --gres=gpu:v100l:1 --time=0-1:0:0 --account=def-uofavis-ab --cpus-per-task=8 --mem-per-cpu=6GB
 
 #allocate any GPU
 salloc --gres=gpu:1 --time=0-1:0:0 --account=def-uofavis-ab --cpus-per-task=8 --mem-per-cpu=6GB
->>>>>>> 9c11277707707171491755eb1d4aa8242b4ffafd
