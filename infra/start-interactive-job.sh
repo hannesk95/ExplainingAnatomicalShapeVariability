e3ec2380a76addf68aa3279a5d7177bdfbb22ef7
Johannes Kiechle <hannes.kiechle@gmail.com>
2022-11-11 14:09:31 +0100

    clean up repository

--- infra/start-interactive-job.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-#allocate V100 GPU
-#salloc --gres=gpu:v100l:1 --time=0-1:0:0 --account=def-uofavis-ab --cpus-per-task=8 --mem-per-cpu=6GB
-
-#allocate any GPU
-salloc --gres=gpu:1 --time=0-1:0:0 --account=def-uofavis-ab --cpus-per-task=8 --mem-per-cpu=6GB
