1e80fe5349806d508ab3dc22ea2de5c3cc003572
Johannes Kiechle <hannes.kiechle@gmail.com>
2022-11-21 13:02:46 +0100

    update repo structure

--- src/ShapeWorks/hippocampus/main.py
+++ src/ShapeWorks/hippocampus/main.py
@@ -9,13 +9,13 @@ from tqdm import tqdm
 
 
 def preprocessing(args):
-    """Data preprocessing."""
-    data_dir = 'data/'
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+    """Preprocess data."""
+    # data_dir = 'data/'
+    # if not os.path.exists(data_dir):
+    #     os.makedirs(data_dir)
 
-    for segmentation in tqdm(glob(f"{data_dir}*.nii")):
-        filename = segmentation.split(".")[0]
+    for segmentation in tqdm(glob(f"{args.data_dir}*.nii")):
+        filename = segmentation.split("\\")[-1].split(".")[0].replace("_standard", "")
 
         # Read binary segmentation and split into left and right
         seg = sw.Image(segmentation)
@@ -43,17 +43,19 @@ def preprocessing(args):
         mesh_left.smooth(3, 1)
         mesh_right.smooth(3, 1)
 
-        mesh_left.write(f"{filename}_left.vtk")
-        mesh_right.write(f"{filename}_right.vtk")
+        mesh_left.write(f"{args.output_dir}{filename}_left.vtk")
+        mesh_right.write(f"{args.output_dir}{filename}_right.vtk")
 
 
 def grooming(args):
     """Groom data."""
-    groom_dir = 'groomed/'
-    if not os.path.exists(groom_dir):
-        os.makedirs(groom_dir)
+    # groom_dir = 'groomed/'
+    # if not os.path.exists(groom_dir):
+    #     os.makedirs(groom_dir)
 
-    mesh_files = glob(f"{args.data_dir}*.vtk")
+    groom_dir = args.output_dir
+
+    mesh_files = glob(f"{args.output_dir}*.vtk")
 
     # list of shape segmentations
     mesh_list = []
@@ -63,7 +65,7 @@ def grooming(args):
     for mesh_file in mesh_files:
         print('Loading: ' + mesh_file)
         # get current shape name
-        mesh_name = mesh_file.split('/')[-1].replace('.vtk', '')
+        mesh_name = mesh_file.split('\\')[-1].replace('.vtk', '')
         mesh_names.append(mesh_name)
         # get domain identifiers
         domain_ids.append(mesh_name.split(".")[0].split("_")[-1])
@@ -81,6 +83,7 @@ def grooming(args):
     domain2_indx = list(np.where(domain_ids == 'right')[0])
 
     # Select reference for rigid alignment
+    print("[INFO] Looking for reference (medoid) shape ...")
     domains_per_shape = 2
     domain_1_meshes = []
     # get domain 1 shapes
@@ -108,8 +111,13 @@ def grooming(args):
             print('Aligning ' + name + ' to ' + ref_name[d])
             transforms.append(rigidTransform)
 
+    # Delete old groomed meshes
+    delete_meshes = glob(f"{args.output_dir}*.vtk")
+    for file in delete_meshes:
+        os.remove(file)
+
     # Save groomed meshes
-    groomed_mesh_files = sw.utils.save_meshes(groom_dir + 'meshes/', mesh_list, mesh_names, extension='vtk')
+    groomed_mesh_files = sw.utils.save_meshes(groom_dir, mesh_list, mesh_names, extension='vtk')
 
     return domain_1_meshes, mesh_files, groomed_mesh_files, transforms
 
@@ -188,7 +196,12 @@ def run_shapeworks_pipeline(args) -> None:
 
     preprocessing(args)
     domain_1_meshes, mesh_files, groomed_mesh_files, transforms = grooming(args)
-    particle_optimization(args, domain_1_meshes, mesh_files, groomed_mesh_files, transforms)
+
+    particles_list = [2048, 4096]
+    for particles in particles_list:
+        args.project_location = f"{args.project_location}_{str(particles)}"
+        args.particles = particles
+        particle_optimization(args, domain_1_meshes, mesh_files, groomed_mesh_files, transforms)
 
     if args.studio:
         run_studio()
@@ -197,14 +210,20 @@ def run_shapeworks_pipeline(args) -> None:
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='ShapeWorks Pipeline')
-    parser.add_argument('--data_dir', type=str, default='data/')
-    parser.add_argument('--project_location', type=str, default='shape_model/')
+    parser.add_argument('--data_dir', type=str, default='binary_segmentations/')
+    parser.add_argument('--output_dir', type=str, default='groomed/')
+    parser.add_argument('--project_location', type=str, default='shape_model')
     parser.add_argument('--spreadsheet_name', type=str, default='hippocampus.xlsx')
     parser.add_argument('--particles', type=int, default=128)
     parser.add_argument('--studio', type=bool, default=False)
     parser.add_argument('--studio_only', type=bool, default=False)
     parser.add_argument('--domains_per_shape', type=int, default=2)
     args = parser.parse_args()
+
+    #args.project_location = f"{args.project_location}_{str(args.particles)}"
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
     if args.studio_only:
         run_studio()
